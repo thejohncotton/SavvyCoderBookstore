993b3e8dad0e339b16875179dd33e851dba2b5b7
pet <nannayarn @gmail.com>
2018-11-13 19:44:51 -0600

    Form imputs added

--- components/Form.js
+++ components/Form.js
@@ -1,7 +1,13 @@
 export default `
 <form>
-<input type="text">
-<input type="text">
+<input type="text" placeholder="Title" name="title">
+<input type="text" placeholder="Author" name="author">
+<input type="text" placeholder="Image URL" name="image-url">
+<input type="text" placeholder="Price" name="price">
+<input type="text" placeholder="Selling point" name="selling-point-1">
+<input type="text" placeholder="Selling point" name="selling-point-2">
+<input type="text" placeholder="Selling point" name="selling-point-3">
+
 <input type="submit">
 </form>
 `
